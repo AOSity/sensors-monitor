76d0ed6b3d7b91231ee53748af33708a56840745
Andrii Horbul <andreyhorbggwp@gmail.com>
2025-02-19 00:16:28 +0200

    Generate individual files for peripherals

--- target/NUCLEO-F767ZI/Core/Src/freertos.c
+++ target/NUCLEO-F767ZI/Core/Src/freertos.c
@@ -21,6 +21,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -46,11 +47,167 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for GreenLed */
+osThreadId_t GreenLedHandle;
+const osThreadAttr_t GreenLed_attributes = {
+  .name = "GreenLed",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for BlueLed */
+osThreadId_t BlueLedHandle;
+const osThreadAttr_t BlueLed_attributes = {
+  .name = "BlueLed",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for RedLed */
+osThreadId_t RedLedHandle;
+const osThreadAttr_t RedLed_attributes = {
+  .name = "RedLed",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
 /* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void green_led_task(void *argument);
+void blue_led_task(void *argument);
+void red_led_task(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of GreenLed */
+  GreenLedHandle = osThreadNew(green_led_task, NULL, &GreenLed_attributes);
+
+  /* creation of BlueLed */
+  BlueLedHandle = osThreadNew(blue_led_task, NULL, &BlueLed_attributes);
+
+  /* creation of RedLed */
+  RedLedHandle = osThreadNew(red_led_task, NULL, &RedLed_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_green_led_task */
+/**
+* @brief Function implementing the GreenLed thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_green_led_task */
+void green_led_task(void *argument)
+{
+  /* USER CODE BEGIN green_led_task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END green_led_task */
+}
+
+/* USER CODE BEGIN Header_blue_led_task */
+/**
+* @brief Function implementing the BlueLed thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_blue_led_task */
+void blue_led_task(void *argument)
+{
+  /* USER CODE BEGIN blue_led_task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END blue_led_task */
+}
+
+/* USER CODE BEGIN Header_red_led_task */
+/**
+* @brief Function implementing the RedLed thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_red_led_task */
+void red_led_task(void *argument)
+{
+  /* USER CODE BEGIN red_led_task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END red_led_task */
+}
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
